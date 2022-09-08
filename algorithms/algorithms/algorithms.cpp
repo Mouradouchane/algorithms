2b83aad92a71d9b087dab4d102d45608eba7baa4
Mouradouchane <mouradouchane@gmail.com>
2022-09-08 10:28:03 +0100

    add:quick_sort but not stable yet

--- algorithms/algorithms/algorithms.cpp
+++ algorithms/algorithms/algorithms.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
-//#include "sort/linear_sort.hpp"
-//#include "sort/hierarchical_sorting.hpp"
+/*
+#include "sort/hierarchical_sorting.hpp"
 #include "sort/heap_sort.hpp"
+#include "sort/linear_sort.hpp"
 
-//#include "search/binary_search.hpp"
-//#include "search/linear_search.hpp"
+#include "search/binary_search.hpp"
+#include "search/linear_search.hpp"
+*/
+
+#include "sort/quick_sort.hpp"
 
 template<typename T> void print_array(T* arr, size_t const& length);
 
+bool cp_fn( int const& a, int const& b ) {
+    return a < b;
+}
+
 int main(){
 
-    int arr[12] = { 144,8,10,3,9,42,55,100,7,56,2,13 };
+    int arr[6] = { 144,8,10,3,9,1 };
 
     print_array<int>(arr, (sizeof(arr) / sizeof(int)));
 
-    sort::heap_sort<int>(arr, (size_t)(sizeof(arr) / sizeof(int)) - 1, 
-        []( int const& a , int const& b ) -> bool { return a > b; } 
-    );
+    sort::quick_sort<int>(arr, 0 , (int)(sizeof(arr) / sizeof(int)) - 1 , &cp_fn );
 
     print_array<int>( arr , (sizeof(arr) / sizeof(int)) );
 
--- algorithms/algorithms/sort/quick_sort.hpp
+++ algorithms/algorithms/sort/quick_sort.hpp
@@ -1,1 +1,87 @@
+
 #pragma once
+
+namespace sort {
+
+	// private namespace for quick sort stuff
+	namespace {
+
+		// O( 1 )
+		template<typename T> void swap( T& a , T& b ) {
+
+			T temp = a;
+			a = b;
+			b = temp;
+
+		}
+
+		// O( n )
+		template<typename T> int parition( 
+			T* arr , 
+			int left , 
+			int right , 
+			bool (*&compare_function)(T const& a, T const& b)
+		) {
+
+			// "pivot"
+			int pv = (left + right) / 2;
+			int i = left , j = right;
+
+			while ( i <= j ) {
+
+
+				while (i <= right) {
+					if (compare_function(arr[pv], arr[i])) i += 1;
+				}
+
+				while (j >= left) {
+					if( !compare_function(arr[pv], arr[j]) ) j -= 1;
+				}
+
+				if (j < left) {
+					T temp = arr[left];
+					arr[left] = arr[pv];
+					arr[pv] = temp;
+			
+					return left;
+				}
+				else {
+
+					T temp = arr[j+1];
+					arr[j+1] = arr[pv];
+					arr[pv] = temp;
+
+					return j+1;
+
+				}
+
+			}
+
+		}
+
+
+	}
+
+
+	// O( n log n )
+	template<typename type> void quick_sort( 
+		type * arr ,
+		int left ,
+		int right ,
+		bool (*compare_function)(type const& a, type const& b)
+	){
+
+		if (left < right) {
+
+			int partiton_index = parition<type>(arr, left, right , compare_function);
+
+			// preforme quick sort recursivlly in each direction
+			quick_sort<type>(arr, left, partiton_index - 1 , compare_function);
+			quick_sort<type>(arr, partiton_index + 1 , right , compare_function);
+
+		}
+
+	}
+
+
+} // end of namespace sort
