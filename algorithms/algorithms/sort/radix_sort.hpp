532e2668e2dab36c79e59d1f700f43d0c3f6cd99
Mouradouchane <mouradouchane@gmail.com>
2022-09-16 23:24:56 +0100

    add:radix_sort algorithm basics , but not stable yet

--- algorithms/algorithms/sort/radix_sort.hpp
+++ algorithms/algorithms/sort/radix_sort.hpp
@@ -1,1 +1,46 @@
+#include <vector>
+
 #pragma once
+
+namespace sort {
+
+	void radix_sort(unsigned int* arr , size_t length) {
+
+		std::vector<unsigned int> pins[10];
+		unsigned int max_value = 0;
+
+		unsigned int index = 0;
+		for (unsigned int i = 0; i < length; i += 1) {
+
+			if (arr[i] > max_value) max_value = arr[i];
+
+			index = arr[i] / i % 10;
+
+			pins[index].push_back(arr[i]);
+
+		}
+
+		for (unsigned int i = 0 , p = 0; i < 10; i += 1) {
+			
+			for (unsigned int c = 0; c < pins[i].size(); c += 1 ) {
+				arr[p] = pins[i][c];
+				pins[i][c] = NULL;
+				p += 1;
+			}
+
+		}
+		//======================================
+		index = 0;
+		for (unsigned int i = 0; i < length; i += 1) {
+
+			if (arr[i] > max_value) max_value = arr[i];
+
+			index = arr[i] / i % 10;
+
+			pins[index].push_back(arr[i]);
+
+		}
+
+	}
+
+}
